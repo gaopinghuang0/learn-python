b44128dcecda78c549e892c11ef31a56080e908e
gaopinghuang0 <gaopinghuang@gmail.com>
2015-12-07 21:33:10 -0500

    add basic server and client socket

---
+++ tsTclnt.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python2.7
+# -*- coding: utf-8 -*-
+from __future__ import division, unicode_literals # boilerplate
+from socket import *
+
+HOST = 'localhost'
+PORT = 21567
+BUGSIZ = 1024
+ADDR = (HOST, PORT)
+
+
+def main():
+	tcpCliSock = socket(AF_INET, SOCK_STREAM)
+	tcpCliSock.connect(ADDR)
+
+	while True:
+		data = raw_input('> ')
+		if not data:
+			break
+		tcpCliSock.send(data)
+		data = tcpCliSock.recv(BUGSIZ)
+		if not data:
+			break
+		print data
+
+	tcpCliSock.close()
+
+if __name__=='__main__':
+	main()
