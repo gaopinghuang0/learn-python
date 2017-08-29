5b6dc0113b8b321cb519833a964d43b1c13089bd
gaopinghuang0 <gaopinghuang@gmail.com>
2017-08-29 00:52:04 -0400

    26 remove duplicates from sorted array -- done

---
+++ leetcode/0026_Remove_Duplicates_from_Sorted_Array.py
@@ -0,0 +1,47 @@
+import unittest
+
+
+class Solution(object):
+  def removeDuplicates(self, nums):
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+    # idea: two-pointer
+    if not nums:
+      return 0
+
+    i,j = 0,1
+    n = len(nums)
+    last = nums[i]
+    while j < n:
+      while j < n and last >= nums[j]:
+        j += 1
+      if i+1 < n and j < n:
+        i += 1
+        nums[i] = nums[j]
+        last = nums[i]
+      j += 1
+    return i+1
+
+
+    
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.s = Solution()
+
+  def test_method(self):
+    """Such as self.assertEqual, self.assertTrue"""
+    self.assertEqual(self.s.removeDuplicates([]), 0)
+    self.assertEqual(self.s.removeDuplicates([1]), 1)
+    self.assertEqual(self.s.removeDuplicates([1,1]), 1)
+    self.assertEqual(self.s.removeDuplicates([1,2]), 2)
+    self.assertEqual(self.s.removeDuplicates([1,1,2]), 2)
+    self.assertEqual(self.s.removeDuplicates([1,1,2,2,3]), 3)
+    self.assertEqual(self.s.removeDuplicates([1,2,2,2,3]), 3)
+
+
+if __name__ == "__main__":
+  unittest.main()
