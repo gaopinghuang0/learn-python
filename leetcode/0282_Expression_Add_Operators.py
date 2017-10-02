7268dc621cc11c2a4c78407f256b5128563d58cb
gaopinghuang0 <gaopinghuang@gmail.com>
2017-10-02 10:25:33 -0400

    0282_Expression_Add_Operators.py -- modified

--- leetcode/0282_Expression_Add_Operators.py
+++ leetcode/0282_Expression_Add_Operators.py
@@ -13,7 +13,15 @@ class Solution(object):
     self.n = len(num)
     self.target = target
     self.res = []
-    self.dfs("", 0, 0, 0)
+    # do not add '+', '-', '*' before pos 0
+    # the only possible operation is 'concat'
+    # this for-loop handles the concat before pos
+    # while the for-loop in dfs() handles concat after pos
+    for pos in range(1, self.n+1):
+      if pos > 1 and num[0] == '0': # avoid 00*
+        break
+      curr = int(num[:pos])
+      self.dfs(num[:pos], pos, curr, curr)
     return self.res
 
   def dfs(self, path, pos, val, multed):
@@ -21,26 +29,14 @@ class Solution(object):
       if val == self.target:
         self.res.append(path)
     for i in range(pos+1, self.n+1):
-      if i != pos+1 and self.num[pos] == '0':  # avoid 00*
+      if i != pos+1 and self.num[pos] == '0':  
         break
-
-      curr = int(self.num[pos:i])
-      # do not add '+', '-', '*' before pos 0
-      # the only possible operation is 'concat'
-      # also, since the for-loop guarantees that all possible concat after pos will be handled, 
-      # then when pos != 0, we cannot concat path with curr again.
-      # for example, num is '12345' and pos is 2, then the for-loop will call path on
-      # '12->3', '12->34', and '12->345' in the subcalls, respectively.
-      # Note that in the subcall of path '12->3', if we concat again,
-      # we will get path '12->34', '12->345' again, which is duplicate 
-      if pos == 0:
-        self.dfs(path+str(curr), i, curr, curr)
-      else:
-        # self.dfs(path+str(curr), i, ..., ...)  # <-- note: cannot concat again
-        self.dfs(path+'+'+str(curr), i, val+curr, curr)
-        self.dfs(path+'-'+str(curr), i, val-curr, -curr)
-        self.dfs(path+'*'+str(curr), i, val-multed+multed*curr, multed*curr)
-      
+      s = self.num[pos:i]
+      curr = int(s)
+      self.dfs(path+'+'+s, i, val+curr, curr)
+      self.dfs(path+'-'+s, i, val-curr, -curr)
+      self.dfs(path+'*'+s, i, val-multed+multed*curr, multed*curr)
+    
 
 class TestSolution(unittest.TestCase):
   def setUp(self):
