ce73ca82bd2aeb132e3e91058f6c2e4358b79fbb
gaopinghuang0 <gaopinghuang@gmail.com>
2017-10-05 21:22:31 -0400

    0636_Exclusive_Time_of_Functions.py -- done but not concise

---
+++ leetcode/0636_Exclusive_Time_of_Functions.py
@@ -0,0 +1,68 @@
+import unittest
+
+from collections import defaultdict
+class Solution(object):
+    def exclusiveTime(self, n, logs):
+        """
+        :type n: int
+        :type logs: List[str]
+        :rtype: List[int]
+        """
+        c = [0] * n
+        stack = []
+        last_time = 0
+        for log in logs:
+            _id, task, time = log.split(":")
+            _id, time = int(_id), int(time)
+            if not stack:
+                if task == 'start':
+                    stack.append(_id)
+                    last_time = time
+                else:
+                    raise ValueError
+            else:
+                last_id = stack[-1]
+                if _id == last_id:
+                    if task == 'end':
+                        c[_id] += time - last_time + 1
+                        stack.pop(-1)
+                        last_time = time + 1
+                    else:
+                        c[_id] += time - last_time
+                        stack.append(_id)
+                        last_time = time
+                elif _id != last_id:
+                    if task == 'start':
+                        c[last_id] += time - last_time
+                        stack.append(_id)
+                        last_time = time
+                    else:
+                        raise ValueError
+        return c
+
+
+
+
+
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_method(self):
+        """Such as self.assertEqual, self.assertTrue"""
+        logs = ["0:start:0",
+             "1:start:2",
+             "1:end:5",
+             "0:end:6"]
+        self.assertEqual(self.s.exclusiveTime(2, logs), [3, 4])
+        logs = ["0:start:0","0:start:2","0:end:5","0:start:6","0:end:6","0:end:7"]      
+        self.assertEqual(self.s.exclusiveTime(1, logs), [8])
+        logs = ["0:start:0","1:start:4","2:start:8","3:start:12","4:start:17","4:end:711","3:end:714","2:end:718","5:start:722","6:start:723","6:end:726","7:start:728","8:start:733","8:end:736","7:end:738","5:end:742","1:end:747","0:end:749"]
+        self.assertEqual(self.s.exclusiveTime(9, logs), [8])
+
+
+
+if __name__ == "__main__":
+    unittest.main()
