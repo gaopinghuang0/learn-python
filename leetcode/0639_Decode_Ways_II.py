4b17c0feb28ffa01ec7c852bfde60e5a809eb708
gaopinghuang0 <gaopinghuang@gmail.com>
2017-10-12 23:01:09 -0400

    0639_Decode_Ways_II.py -- optimize

--- leetcode/0639_Decode_Ways_II.py
+++ leetcode/0639_Decode_Ways_II.py
@@ -1,4 +1,60 @@
 
+# optimize
+class Solution(object):
+  def numDecodings(self, s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    # idea: just save the prev two counts
+    if not s:
+      return 0
+    modular = 10**9 + 7
+    prev_2 = 1
+    prev_1 = 1
+    x = s[0]
+    if x == '*':
+      prev_1 = 9
+    elif x == '0':
+      return 0
+    else:
+      prev_1 = 1
+    
+    for i, x in enumerate(s[1:], 1):
+      n = 0
+      prev = s[i-1]
+      if x == '0':
+        if prev == '1' or prev == '2':
+          n += prev_2
+        elif prev == '*':
+          n += prev_2 * 2
+        else:
+          return 0
+      elif x == '*':
+        n += prev_1 * 9
+        if prev == '1':
+          n += prev_2 * 9
+        elif prev == '2':
+          n += prev_2 * 6
+        elif prev == '*':
+          n += prev_2 * 15
+      else:  # '1'-'9'
+        n += prev_1
+        if prev == '1':
+          n += prev_2
+        elif prev == '2' and x <= '6':
+          n += prev_2
+        elif prev == '*' and x <= '6':
+          n += prev_2 * 2
+        elif prev == '*' and x > '6':
+          n += prev_2
+      prev_2 = prev_1
+      prev_1 = n % modular
+    return prev_1 % modular
+
+
+
+# correct but a little slow
 class Solution(object):
   def numDecodings(self, s):
     """
