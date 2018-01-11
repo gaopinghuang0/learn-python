38eb4bc5949b17de36b3a037d44917c46724b566
gaopinghuang0 <gaopinghuang@gmail.com>
2018-01-10 23:21:31 -0500

    0058_Length_of_Last_Word.py -- done, beats 29.50%

---
+++ leetcode/0058_Length_of_Last_Word.py
@@ -0,0 +1,24 @@
+import unittest
+
+
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        return len(s.strip().split(' ')[-1])
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.s = Solution()
+
+  def test_method(self):
+    """Such as self.assertEqual, self.assertTrue"""
+    # self.assertEqual(self.s.method(), None)
+    pass
+
+
+if __name__ == "__main__":
+  unittest.main()
