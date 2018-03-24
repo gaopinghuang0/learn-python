5dafa87394514990f83de1b17f07dade86e73867
gaopinghuang0 <gaopinghuang@gmail.com>
2018-03-24 18:46:58 -0400

    0018_4Sum.py -- beats 48.03%

---
+++ leetcode/0018_4Sum.py
@@ -0,0 +1,62 @@
+import unittest
+
+
+
+# beats 48.03%
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        res = []
+        for i in range(len(nums)-3):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            for three in self.threeSum(nums[i+1:], target-nums[i]):
+                res.append([nums[i]] + three)
+        return res
+
+    def threeSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # copied from 15_3Sum
+        res = []
+        size = len(nums)
+        for i in range(size-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, size-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < target:
+                    l += 1
+                elif s > target:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
+        return res
+
+
+class TestSolution(unittest.TestCase):
+  def setUp(self):
+    self.s = Solution()
+
+  def test_method(self):
+    """Such as self.assertEqual, self.assertTrue"""
+    # self.assertEqual(self.s.method(), None)
+    pass
+
+
+if __name__ == "__main__":
+  unittest.main()
