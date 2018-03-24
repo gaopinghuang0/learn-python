2324df4fc90b34b13b2009522eca36e811ba3013
gaopinghuang0 <gaopinghuang@gmail.com>
2018-03-24 18:32:05 -0400

    update template.py

--- leetcode/.tmp/template.py
+++ leetcode/.tmp/template.py
@@ -2,18 +2,18 @@ import unittest
 
 
 class Solution(object):
-  pass
-
-
-class TestSolution(unittest.TestCase):
-  def setUp(self):
-    self.s = Solution()
-
-  def test_method(self):
-    """Such as self.assertEqual, self.assertTrue"""
-    # self.assertEqual(self.s.method(), None)
     pass
 
 
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_method(self):
+        """Such as self.assertEqual, self.assertTrue"""
+        # self.assertEqual(self.s.method(), None)
+        pass
+
+
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
