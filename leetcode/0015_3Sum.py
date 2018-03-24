8fc965d187ef45770a3618f745e5941f7eac365d
gaopinghuang0 <gaopinghuang@gmail.com>
2018-03-23 23:56:57 -0400

    0015_3Sum.py -- optimize by other's submission, beats 99.91%

--- leetcode/0015_3Sum.py
+++ leetcode/0015_3Sum.py
@@ -1,33 +1,66 @@
 import unittest
 
+# optimize based on other's submission
+# beats 99.91%
+from collections import defaultdict
 class Solution(object):
-  def threeSum(self, nums):
-    """
-    :type nums: List[int]
-    :rtype: List[List[int]]
-    """
-    res = []
-    nums.sort()
-    size = len(nums)
-    for i in xrange(size-2):
-      if i > 0 and nums[i] == nums[i-1]:
-        continue
-      l, r = i+1, size-1
-      while l < r:
-        s = nums[i] + nums[l] + nums[r]
-        if s < 0:
-          l += 1
-        elif s > 0:
-          r -= 1
-        else:
-          res.append([nums[i], nums[l], nums[r]])
-          l += 1
-          r -= 1
-          while l < r and nums[l] == nums[l-1]:
-            l += 1
-          while l < r and nums[r] == nums[r+1]:
-            r -= 1
-    return res
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        count = defaultdict(int)
+        for num in nums:
+                count[num] += 1
+
+        res = []
+        if 0 in count and count[0] > 2:
+            res.append([0,0,0])
+
+        keys = count.keys()
+        pos = sorted([k for k in keys if k > 0])
+        neg = sorted([k for k in keys if k < 0])
+
+        for p in pos:
+            for n in neg:
+                other = -(p+n)
+                if other in count:
+                    if other == p and count[p] > 1:
+                        res.append([n,p,p])
+                    elif other == n and count[n] > 1:
+                        res.append([n,n,p])
+                    elif n < other < p:
+                        res.append([n, other, p])
+        return res
+
+class Solution_V1(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        nums.sort()
+        size = len(nums)
+        for i in xrange(size-2):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, size-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < 0:
+                    l += 1
+                elif s > 0:
+                    r -= 1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
+        return res
 
 
 # time limit exceeded
@@ -64,24 +97,24 @@ class Solution(object):
 
 
 class TestSolution(unittest.TestCase):
-  def setUp(self):
-    self.s = Solution()
+    def setUp(self):
+        self.s = Solution()
 
-  def test_method(self):
-    """Such as self.assertEqual, self.assertTrue"""
-    nums = [-1, 0, 1, 2, -1, -4]
-    print self.s.threeSum(nums)
-    print [[-1, 0, 1],  [-1, -1, 2]]
+    def test_method(self):
+        """Such as self.assertEqual, self.assertTrue"""
+        nums = [-1, 0, 1, 2, -1, -4]
+        print self.s.threeSum(nums)
+        print [[-1, 0, 1],  [-1, -1, 2]]
 
-    nums = [-1, 2, -1, -1, -4]
-    print self.s.threeSum(nums)
-    print [[-1, 2, -1]]
+        nums = [-1, 2, -1, -1, -4]
+        print self.s.threeSum(nums)
+        print [[-1, 2, -1]]
 
 
-    nums = [-1, 2, -1, 2, -4]
-    print self.s.threeSum(nums)
-    print [[-1, 2, -1], [2, 2, -4]]
+        nums = [-1, 2, -1, 2, -4]
+        print self.s.threeSum(nums)
+        print [[-1, 2, -1], [2, 2, -4]]
 
 
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
