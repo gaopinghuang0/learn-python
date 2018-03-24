ebb20e1a1e3c095bdf443280ef604eafafb1e12d
gaopinghuang0 <gaopinghuang@gmail.com>
2018-03-24 19:04:14 -0400

    optimize with other's submission, beats 93.39%

--- leetcode/0018_4Sum.py
+++ leetcode/0018_4Sum.py
@@ -1,5 +1,37 @@
 import unittest
 
+# optimize by other's submission, beats 93.39%
+class Solution(object):
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        def findNsum(nums, target, N, path, results):
+            if len(nums) < N or N < 2 or target < nums[0]*N or target > nums[-1]*N:  # early termination
+                return
+            if N == 2: # two pointers for sorted 2-sum
+                l, r = 0, len(nums)-1
+                while l < r:
+                    s = nums[l] + nums[r]
+                    if s == target:
+                        results.append(path+[nums[l], nums[r]])
+                        l += 1
+                        while l < r and nums[l] == nums[l-1]:
+                            l += 1
+                    elif s < target:
+                        l += 1
+                    else:
+                        r -= 1
+            else:  # recursively reduce N
+                for i in range(len(nums)-N+1):
+                    if i == 0 or nums[i-1] != nums[i]:
+                        findNsum(nums[i+1:], target-nums[i], N-1, path+[nums[i]], results)
+
+        results = []
+        findNsum(sorted(nums), target, 4, [], results)
+        return results
 
 
 # beats 48.03%
