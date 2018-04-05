c30663129c8568fa0f6d87b8660fca6c54c66950
gaopinghuang0 <gaopinghuang@gmail.com>
2018-04-05 00:00:05 -0400

    0448_Find_All_Numbers_Disappeared_in_an_Array.py -- done, beats 7.79%

---
+++ leetcode/0448_Find_All_Numbers_Disappeared_in_an_Array.py
@@ -0,0 +1,23 @@
+
+# beats 7.79%
+class Solution(object):
+    def findDisappearedNumbers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # idea: turn visited postive into negative, and stay negative if visit twice
+        n = len(nums)
+        for i in range(n):
+            x = abs(nums[i]) - 1
+            if x < n and nums[x] > 0:
+                nums[x] *= -1
+        res = []
+        for i in range(n):
+            if nums[i] > 0:
+                res.append(i+1)
+        return res
+
+obj = Solution()
+print(obj.findDisappearedNumbers([4,3,2,7,8,2,3,1]))   # [5,6]
+
