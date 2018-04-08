152219dbfa124b1740254c9d8c8faefc2b633b41
gaopinghuang0 <gaopinghuang@gmail.com>
2018-04-07 22:47:49 -0400

    0118_Pascal_Triangle.py -- faster, beats 95.95%

--- leetcode/0118_Pascal_Triangle.py
+++ leetcode/0118_Pascal_Triangle.py
@@ -1,3 +1,21 @@
+# another (similar) idea from Submission
+# beats 95.95%
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows==0:
+            return []
+        res=[[1]]
+        for i in range(1,numRows):
+            item=[1]
+            for j in range(1,i):
+                item.append(res[i-1][j-1]+res[i-1][j])
+            item.append(1)
+            res.append(item)
+        return res
 
 # beats 8.11%
 class Solution(object):
