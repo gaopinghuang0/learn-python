144c6ce1d38be7a3eecb3f1a29b8b74ad011946e
gaopinghuang0 <gaopinghuang@gmail.com>
2018-04-08 21:48:28 -0400

    0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py -- done, beats 8.85%

---
+++ leetcode/0106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -0,0 +1,44 @@
+import unittest
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# beats 8.85%
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        if not postorder or not inorder:
+            return None
+        if len(postorder) == 1:
+            return TreeNode(postorder[0])
+        last = postorder[-1]
+        root = TreeNode(last)
+        for i,val in enumerate(inorder):
+            if val == last:
+                break
+        root.left = self.buildTree(inorder[:i], postorder[:i])
+        root.right = self.buildTree(inorder[i+1:], postorder[i:-1])
+        return root
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_method(self):
+        """Such as self.assertEqual, self.assertTrue"""
+        # self.assertEqual(self.s.method(), None)
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
