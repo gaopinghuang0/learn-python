f9361476ae80ce476bf27054a714767e559f6edf
gaopinghuang0 <gaopinghuang@gmail.com>
2018-04-10 23:01:10 -0400

    0084_Largest_Rectangle_in_Histogram.py -- add another solution

--- leetcode/0084_Largest_Rectangle_in_Histogram.py
+++ leetcode/0084_Largest_Rectangle_in_Histogram.py
@@ -1,7 +1,44 @@
 import unittest
 
-# idea from Discuss, beats 56.97%
+# idea from Discuss, beats 16.08%
 class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        # idea from Discuss, using a stack
+        # note: the basic idea is very similar to the Solution_V1 below
+        # i.e., try to find the 'left index' and 'right index' efficiently
+        # so that the width is (r-l-1), the difference is that we are using a stack
+
+        # for any bar i, if the bar is higher than the bar at the stack top, push i to stack
+        # if it's lower, then keep poping stack until reach smaller bar. For each removed bar
+        # from the stack, say heights[tp], calculate the area of rect with heights[tp] as the
+        # smallest bar, the 'left index' is previous item in stack and 'right index'
+        # is 'i' (currect index)
+        stack = []
+        res = 0
+        n = len(heights)
+        for i, h in enumerate(heights):
+            if not stack or h >= heights[stack[-1]]:
+                stack.append(i)
+            else:
+                while stack and h < heights[stack[-1]]:
+                    tp = stack.pop()
+                    left = stack[-1] if stack else -1
+                    res = max(res, heights[tp] * (i - left - 1))
+                stack.append(i)
+        # some elements remaining, now repeat the same process as above
+        while stack:
+            tp = stack.pop()
+            left = stack[-1] if stack else -1
+            res = max(res, heights[tp] * (n - left - 1))
+        return res
+
+
+# idea from Discuss, beats 56.97%
+class Solution_V1(object):
     def largestRectangleArea(self, heights):
         """
         :type heights: List[int]
@@ -81,6 +118,7 @@ class TestSolution(unittest.TestCase):
 
     def test_method(self):
         """Such as self.assertEqual, self.assertTrue"""
+        self.assertEqual(self.s.largestRectangleArea([]), 0)
         self.assertEqual(self.s.largestRectangleArea([2,1,5,6,2,3]), 10)
         self.assertEqual(self.s.largestRectangleArea([999,999,999,999]), 3996)
         self.assertEqual(self.s.largestRectangleArea([3,6,5,7,4,8,1,0]), 20)
