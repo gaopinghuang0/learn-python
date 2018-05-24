f91edc1a85d0f099d750bc08f151839aba1c30d5
gaopinghuang0 <gaopinghuang@gmail.com>
2018-05-24 15:05:49 -0400

    0012_Integer_to_Roman.py -- optimize, beats 32.94%

--- leetcode/0012_Integer_to_Roman.py
+++ leetcode/0012_Integer_to_Roman.py
@@ -1,7 +1,50 @@
 import unittest
 
-# beats 28.95%
+# optimize a little by using global memo, beats 32.94%
 class Solution(object):
+    memo = {}
+    def intToRoman(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        # input is within 1 to 3999
+        symbols = {
+            1: 'I',
+            5: 'V',
+            10: 'X',
+            50: 'L',
+            100: 'C',
+            500: 'D',
+            1000: 'M'
+        }
+        def convert(x, base):
+            if (x,base) in self.memo:
+                return self.memo[x,base]
+            if x == 0:
+                ans = ''
+            elif 1 <= x <= 3:
+                ans = symbols[base]*x
+            elif x == 4:
+                ans = symbols[base]+symbols[5*base]
+            elif 5 <= x <= 8:
+                ans = symbols[5*base]+symbols[base]*(x - 5)
+            elif x == 9:
+                ans = symbols[base]+symbols[10*base]
+            self.memo[(x,base)] = ans
+            return ans
+
+        res = ''
+        base = 1
+        while num:
+            res = convert(num % 10, base) + res
+            num = num // 10
+            base = base * 10
+
+        return res
+
+# beats 28.95%
+class Solution_V1(object):
     def intToRoman(self, num):
         """
         :type num: int
