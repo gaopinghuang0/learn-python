ab1737f16a1c4725872a187717ee0e9689e5e056
gaopinghuang0 <gaopinghuang@gmail.com>
2018-05-31 22:19:28 -0400

    0230_Kth_Smallest_Element_in_a_BST.py -- update, a little slower

--- leetcode/0230_Kth_Smallest_Element_in_a_BST.py
+++ leetcode/0230_Kth_Smallest_Element_in_a_BST.py
@@ -1,5 +1,26 @@
 import unittest
 
+# it's a little slower, but maybe due to unstable leetcode timer
+class Solution(object):
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        # idea: recursion
+        # note that 1 <= k <= # of nodes
+        # get sorted order and then get the kth element
+        def helper(root, nodes):
+            if root.left:
+                helper(root.left, nodes)
+            nodes.append(root.val)
+            if root.right:
+                helper(root.right, nodes)
+        
+        nodes = []
+        helper(root, nodes)
+        return nodes[k-1]
 
 # beats 66.82%
 class Solution(object):
