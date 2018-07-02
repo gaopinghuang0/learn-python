d7ebaf8145a98a4df022a507e02bac5ca189bbc9
gaopinghuang0 <gaopinghuang@gmail.com>
2018-07-01 23:51:41 -0400

    0657_Judge_Route_Circle.py -- done, beats 59.02%

---
+++ leetcode/0657_Judge_Route_Circle.py
@@ -0,0 +1,27 @@
+import unittest
+
+# beats 59.02%
+import collections
+class Solution(object):
+    def judgeCircle(self, moves):
+        """
+        :type moves: str
+        :rtype: bool
+        """
+        # idea: the # of up should equal to # of down, similar for left-vs-right
+        counter = collections.Counter(moves)
+        return counter['U'] == counter['D'] and counter['L'] == counter['R']
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_method(self):
+        """Such as self.assertEqual, self.assertTrue"""
+        # self.assertEqual(self.s.method(), None)
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
