51c345d7469ee2969c02a40ac9652852339031a6
gaopinghuang0 <gaopinghuang@gmail.com>
2018-07-02 20:49:19 -0400

    0617_Merge_Two_Binary_Trees.py -- done, beats 24.83%

---
+++ leetcode/0617_Merge_Two_Binary_Trees.py
@@ -0,0 +1,44 @@
+import unittest
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# beats 24.83%
+class Solution(object):
+    def mergeTrees(self, t1, t2):
+        """
+        :type t1: TreeNode
+        :type t2: TreeNode
+        :rtype: TreeNode
+        """
+        # idea: recursion
+        if t1 and t2:
+            root = TreeNode(t1.val + t2.val)
+            root.left = self.mergeTrees(t1.left, t2.left)
+            root.right = self.mergeTrees(t1.right, t2.right)
+        elif t1:
+            root = t1
+        elif t2:
+            root = t2
+        else:
+            root = None
+        return root
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_method(self):
+        """Such as self.assertEqual, self.assertTrue"""
+        # self.assertEqual(self.s.method(), None)
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
