fe687c9d77ff2eb95ece2b082ac544f9846f1e21
gaopinghuang0 <gaopinghuang@gmail.com>
2018-07-15 20:24:15 -0400

    0669_Trim_a_Binary_Search_Tree.py -- done, beats 92.60%

---
+++ leetcode/0669_Trim_a_Binary_Search_Tree.py
@@ -0,0 +1,41 @@
+import unittest
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+# beats 92.60%
+class Solution(object):
+    def trimBST(self, root, L, R):
+        """
+        :type root: TreeNode
+        :type L: int
+        :type R: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val < L:
+            return self.trimBST(root.right, L, R)
+        elif root.val > R:
+            return self.trimBST(root.left, L, R)
+        root.left = self.trimBST(root.left, L, R)
+        root.right = self.trimBST(root.right, L, R)
+        return root
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_method(self):
+        """Such as self.assertEqual, self.assertTrue"""
+        self.assertEqual(self.s.method(), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
