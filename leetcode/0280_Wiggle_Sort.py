d01d0423ef1a7d89ba02f0d86ad71429c1a599ed
gaopinghuang0 <gaopinghuang@gmail.com>
2018-07-30 23:32:40 -0400

    0280_Wiggle_Sort.py -- done, beats 6.92%

---
+++ leetcode/0280_Wiggle_Sort.py
@@ -0,0 +1,31 @@
+import unittest
+
+# beats 6.92%
+class Solution(object):
+    def wiggleSort(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        up = True
+        n = len(nums)
+        for i in range(1, n):
+            if up and nums[i-1] > nums[i]:
+                nums[i], nums[i-1] = nums[i-1], nums[i]
+            elif not up and nums[i-1] < nums[i]:
+                nums[i], nums[i-1] = nums[i-1], nums[i]
+            up = not up
+
+
+
+class TestSolution(unittest.TestCase):
+    def setUp(self):
+        self.s = Solution()
+
+    def test_method(self):
+        """Such as self.assertEqual, self.assertTrue"""
+        self.assertEqual(self.s.method(), None)
+
+
+if __name__ == "__main__":
+    unittest.main()
